7d59d64d8139ade437a21393b54bfc6d3dd5ae7d
Vladimir <guiderwg@gmail.com>
2021-09-04 13:21:38 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataBlockComponent } from './data-block/data-block.component';
+import { ButtonComponent } from './data-block/button/button.component';
+import { CardComponent } from './data-block/card/card.component';
+import { TableListComponent } from './data-block/table-list/table-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBlockComponent,
+    ButtonComponent,
+    CardComponent,
+    TableListComponent
   ],
   imports: [
     BrowserModule
